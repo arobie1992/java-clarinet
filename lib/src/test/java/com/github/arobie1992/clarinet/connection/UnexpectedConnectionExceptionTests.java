cd8aca89e667785afe2982e55d9c633ebf4ccaac
Andrew Robie <arobie1992@yahoo.com>
2024-05-14 17:37:02 -0400

    UnexpectedConnectionException tests

---
+++ lib/src/test/java/com/github/arobie1992/clarinet/connection/UnexpectedConnectionExceptionTests.java
@@ -0,0 +1,25 @@
+package com.github.arobie1992.clarinet.connection;
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
+
+public class UnexpectedConnectionExceptionTests {
+    private final ConnectionId expected = ConnectionId.random();
+    private final ConnectionId actual = ConnectionId.random();
+    private final UnexpectedConnectionException exception = new UnexpectedConnectionException(expected, actual);
+
+    @Test
+    void testMessage() {
+        assertEquals(String.format("Unexpected connection: expected: %s, actual: %s", expected, actual), exception.getMessage());
+    }
+
+    @Test
+    void testExpected() {
+        assertEquals(expected, exception.expected());
+    }
+
+    @Test
+    void testActual() {
+        assertEquals(actual, exception.actual());
+    }
+}
