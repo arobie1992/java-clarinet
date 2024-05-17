e1fe2b201cadb56925f7e5725665b96bdc746f96
Andrew Robie <arobie1992@yahoo.com>
2024-05-17 12:17:18 -0400

    Test helper for peer IDs

--- lib/src/test/java/com/github/arobie1992/clarinet/connection/ConnectionImplTest.java
+++ lib/src/test/java/com/github/arobie1992/clarinet/connection/ConnectionImplTest.java
@@ -1,8 +1,8 @@
 package com.github.arobie1992.clarinet.connection;
 
-import com.github.arobie1992.clarinet.impl.peer.StringPeerId;
 import com.github.arobie1992.clarinet.peer.PeerId;
-import com.github.arobie1992.clarinet.support.TestConnection;
+import com.github.arobie1992.clarinet.utils.PeerUtils;
+import com.github.arobie1992.clarinet.utils.TestConnection;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.function.ThrowingSupplier;
 import org.junit.jupiter.params.ParameterizedTest;
@@ -17,9 +17,6 @@ import static org.junit.jupiter.api.Assertions.*;
 class ConnectionImplTest {
 
     private static final ConnectionId id = ConnectionId.random();
-    private static final PeerId sender = new StringPeerId("sender");
-    private static final PeerId witness = new StringPeerId("witness");
-    private static final PeerId receiver = new StringPeerId("receiver");
     private static final ConnectionImpl.Status status = ConnectionImpl.Status.REQUESTING_RECEIVER;
 
     @ParameterizedTest()
@@ -35,33 +32,33 @@ class ConnectionImplTest {
 
     @Test
     void testSetWitness() {
-        var connection = new ConnectionImpl(id, sender, receiver, status);
+        var connection = new ConnectionImpl(id, PeerUtils.senderId(), PeerUtils.receiverId(), status);
         connection.lock.writeLock().lock();
-        assertDoesNotThrow(() -> connection.setWitness(witness));
+        assertDoesNotThrow(() -> connection.setWitness(PeerUtils.witnessId()));
         connection.lock.writeLock().unlock();
-        assertEquals(Optional.of(witness), connection.witness());
+        assertEquals(Optional.of(PeerUtils.witnessId()), connection.witness());
     }
 
     @Test
     void testSetWitnessCannotUpdate() {
-        var connection = new ConnectionImpl(id, sender, receiver, status);
+        var connection = new ConnectionImpl(id, PeerUtils.senderId(), PeerUtils.receiverId(), status);
         connection.lock.writeLock().lock();
-        connection.setWitness(witness);
-        var ex = assertThrows(UnsupportedOperationException.class, () -> connection.setWitness(witness));
+        connection.setWitness(PeerUtils.witnessId());
+        var ex = assertThrows(UnsupportedOperationException.class, () -> connection.setWitness(PeerUtils.witnessId()));
         assertEquals("Cannot update witness once it has been set.", ex.getMessage());
         connection.lock.writeLock().unlock();
     }
 
     @Test
     void testSetWitnessThrowsWhenNotWriteLocked() {
-        var connection = new ConnectionImpl(id, sender, receiver, status);
-        var ex = assertThrows(WriteLockException.class, () -> connection.setWitness(witness));
+        var connection = new ConnectionImpl(id, PeerUtils.senderId(), PeerUtils.receiverId(), status);
+        var ex = assertThrows(WriteLockException.class, () -> connection.setWitness(PeerUtils.witnessId()));
         assertEquals("setWitness", ex.operationName());
     }
 
     @Test
     void testSetStatus() {
-        var connection = new ConnectionImpl(id, sender, receiver, status);
+        var connection = new ConnectionImpl(id, PeerUtils.senderId(), PeerUtils.receiverId(), status);
         connection.lock.writeLock().lock();
         assertDoesNotThrow(() -> connection.setStatus(ConnectionImpl.Status.OPEN));
         connection.lock.writeLock().unlock();
@@ -70,18 +67,20 @@ class ConnectionImplTest {
 
     @Test
     void testSetStatusThrowsWhenNotWriteLocked() {
-        var connection = new ConnectionImpl(id, sender, receiver, status);
+        var connection = new ConnectionImpl(id, PeerUtils.senderId(), PeerUtils.receiverId(), status);
         var ex = assertThrows(WriteLockException.class, () -> connection.setStatus(ConnectionImpl.Status.OPEN));
         assertEquals("setStatus", ex.operationName());
     }
 
     private static Stream<Arguments> creationPermutations() {
         return Stream.of(
-                Arguments.of(id, sender, receiver, status, new TestConnection(id, sender, Optional.empty(), receiver, status)),
-                Arguments.of(null, sender, receiver, status, null),
-                Arguments.of(id, null, receiver, status, null),
-                Arguments.of(id, sender, null, status, null),
-                Arguments.of(id, sender, receiver, null, null)
+                Arguments.of(
+                        id, PeerUtils.senderId(), PeerUtils.receiverId(), status,
+                        new TestConnection(id, PeerUtils.senderId(), Optional.empty(), PeerUtils.receiverId(), status)),
+                Arguments.of(null, PeerUtils.senderId(), PeerUtils.receiverId(), status, null),
+                Arguments.of(id, null, PeerUtils.receiverId(), status, null),
+                Arguments.of(id, PeerUtils.senderId(), null, status, null),
+                Arguments.of(id, PeerUtils.senderId(), PeerUtils.receiverId(), null, null)
         );
     }
 
---
+++ lib/src/test/java/com/github/arobie1992/clarinet/utils/PeerUtils.java
@@ -0,0 +1,24 @@
+package com.github.arobie1992.clarinet.utils;
+
+import com.github.arobie1992.clarinet.impl.peer.StringPeerId;
+import com.github.arobie1992.clarinet.peer.PeerId;
+
+public class PeerUtils {
+
+    private static final PeerId senderId = new StringPeerId("sender");
+    private static final PeerId witnessId = new StringPeerId("witness");
+    private static final PeerId receiverId = new StringPeerId("receiver");
+
+    public static PeerId senderId() {
+        return senderId;
+    }
+
+    public static PeerId witnessId() {
+        return witnessId;
+    }
+
+    public static PeerId receiverId() {
+        return receiverId;
+    }
+
+}
